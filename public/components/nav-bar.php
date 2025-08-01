40a7b4c7fdcdfc0e2278c68284e1709148d69b26
DeviCode <jpaquinol44@gmail.com>
2025-08-02 01:41:42 +0800

    added a navbar, landingpage, components, about, and services pages

---
+++ public/components/nav-bar.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-success-subtle">
+    <div class="container-fluid">
+        <a class="navbar-brand text-success-emphasis fs-2 fw-bold fst-italic" href="index.php">SMS</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link <?= $current_page == 'home' ? 'active text-success-emphasis fw-bold fs-5' : '' ?>" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link <?= $current_page == 'services' ? 'active text-success-emphasis fw-bold fs-5' : '' ?>" href="services.php">Services</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link <?= $current_page == 'contact' ? 'active text-success-emphasis fw-bold fs-5' : '' ?>" href="contact.php">Contact Us</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">coming soon</a>
+            </li>
+        </ul>
+        </div>
+        <div class="d-flex gap-3" role="Authentification">
+        <a href="login.php" class="btn btn-outline-success">Login</a>
+        <a href="register.php" class="btn btn-outline-success">Register</a>
+        </div>
+    </div>
+</nav>
--- public/index.php
+++ public/index.php
@@ -3,9 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landing Page | DSMS</title>
+    <?php include 'components/links.php'; ?>
+    <title>Landing Page | SMS</title>
 </head>
 <body>
-    
+
+<section class="nav-section">
+    <?php 
+    $current_page = 'home';
+    include 'components/nav-bar.php'; ?>
+</section>
+
+<section class="section-content mt-5">
+    <div class="container">
+        <h1>Home landing page content</h1>
+        <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
+            <div class="col-lg-6 px-0">
+                <h1 class="display-4 fst-italic">Welcome to DNSU</h1><br>
+                <h1 class="display-4 fst-italic">Online Shop</h1>
+                <p class="lead my-3">DNSU Online Shop is a platform for shopping your favorite DNSU clothings.</p>
+                <p class="lead mb-0"><a href="#" class="text-body-emphasis fw-bold">Continue reading...</a></p>
+            </div>
+        </div>
+    </div>
+</section>
+
 </body>
 </html>
