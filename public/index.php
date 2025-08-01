cd1a198f5a39339664a7d272a6fa7ce2e884f07b
YuKennnn <licokenneth11@gmail.com>
2025-08-02 00:29:05 +0800

    DSMS-1st cumM1T
    oyoyoyoyyoyoyoyyoy

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log in to you Account | DSMS</title>
+    <title>Landing Page | DSMS</title>
 </head>
 <body>
     
