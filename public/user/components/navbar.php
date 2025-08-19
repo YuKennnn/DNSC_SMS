70ee72da06c609db06b09c546fab4d3f2c922cca
YuKennnn <licokenneth11@gmail.com>
2025-08-19 19:59:34 +0800

    some small changes navbar only
    ok

--- public/user/components/navbar.php
+++ public/user/components/navbar.php
@@ -1,6 +1,7 @@
       <link rel="stylesheet" href="../../../public/userCss/navbar.css">
       <nav class="navbar navbar-expand-md bg-light fs-4">
         <div class="container-fluid ms-4 ps-4 ">
+          <img src="../../../public/image/main/dnscLOGO.png" alt="" class="img-fluid me-3 ">
           <a class="navbar-brand fs-5 text-success fw-bold" href="#">Dashboard</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,7 +19,7 @@
             
           </div>
           <div class="d-flex">
-              <img class="btn" src="../../../public/image/main/dnscLOGO.png" type="button" data-bs-toggle="offcanvas"
+              <img class="btn rounded-circle" src="../../public/image/main/mE.png" type="button" data-bs-toggle="offcanvas"
                 data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions"></img>
 
 
--- public/user/main.php
+++ public/user/main.php
@@ -6,19 +6,24 @@
     <title>Document</title>
     <link rel="stylesheet" href="../bootstrap/bootstrap-5.3.7-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../bootstrap/bootstrap-icons-1.13.1/bootstrap-icons.css">
+    <link rel="stylesheet" href="../userCss/main.css">
     <script type="module" src="../bootstrap/bootstrap-5.3.7-dist/js/bootstrap.bundle.min.js"></script>
     <?php include '../user/components/navbar.php'; ?>
-    
-    
 </head>
 <body>
     <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions"
                 aria-labelledby="offcanvasWithBothOptionsLabel">
+            
                 <div class="offcanvas-header">
-                  <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
+                  <h5 class="title">
+                    <img class="img-fluid rounded-circle" src="../../public/image/main/me.png">
+                      YuKennn
+                    </img>
+                  </h5>
                   <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
-                <div class="offcanvas-body col-12 col-lg-6">
+              <h2 class="nav-link"><hr></h2>
+                <div class="offcanvas-body col-6 col-sm-6">
                   <ul class="nav">
                     <li class="nav-item">
                         <a href="" class="nav-link fs-5">My Profile</a>
